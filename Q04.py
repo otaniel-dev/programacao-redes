5507a47d471f62048c9cbe8caeb1100f873845a2
otaniel <otaniel.work2003@gmail.com>
2025-09-19 20:13:26 -0300

    asdcas

--- Q04.py
+++ Q04.py
@@ -6,3 +6,10 @@ while True:
     if ip.lower() == "fim":
         break
     ip_list.append(ip)
+
+arquivo = open("ip.txt","w")
+for ip in ip_list:
+    print(ip)
+    arquivo.write(f"{ip}\n")
+
+arquivo.close()
